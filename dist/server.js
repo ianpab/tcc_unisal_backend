16ab696293a1359c6aed90e76388bd6a60720e43
Ian <ian.pablos@gmail.com>
2020-08-14 19:31:17 -0300

    uploads serverjs

--- dist/server.js
+++ dist/server.js
@@ -8,8 +8,10 @@ dotenv_1.default.config();
 var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 var routes_1 = __importDefault(require("./routes"));
+var uploads_1 = __importDefault(require("../uploads"));
 var app = express_1.default();
 app.use(cors_1.default());
 app.use(express_1.default.json()); // para express entender json
 app.use(routes_1.default);
+app.use(uploads_1.default);
 app.listen(process.env.PORT || 3333);
